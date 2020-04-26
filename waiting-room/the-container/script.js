ca623c002fc972bbc5d536a113b06e62eccb9ada
Isaiah Meek <50851883+Isaiah-DSW@users.noreply.github.com>
2020-04-26 17:22:52 +1200

    Add files via upload

--- waiting-room/the-container/script.js
+++ waiting-room/the-container/script.js
@@ -6,24 +6,24 @@ var containerTop = container.style.top;
 var currentLeft = container.style.left;
 var currentTop = container.style.top;
 while (containerLeft == container.style.left) {
-	containerLeft = Math.round(Math.random() * (container.clientWidth - 1) / 5) + 1 - (container.clientWidth / 2);
+	containerLeft = Math.round(Math.random() * (container.clientWidth - 1) / 5) + 1 - (container.clientWidth / 10);
 }
 while (containerTop == container.style.top) {
-	containerTop = Math.round(Math.random() * (container.clientHeight - 1) / 5) + 1 - (container.clientHeight / 2);
+	containerTop = Math.round(Math.random() * (container.clientHeight - 1) / 5) + 1 - (container.clientHeight / 10);
 }
 var containerDistance = (containerLeft - container.style.left) * (containerLeft - container.style.left) + (containerTop - container.style.top) * (containerTop - container.style.top);
-var time = containerDistance / 200000;
-var size = Math.round(Math.random() * 0.3) + 0.85;
+var time = Math.sqrt(containerDistance) / 30;
+var size = Math.round(Math.random() * 0.5) + 0.8;
 container.style.transition = time + 's linear';
 graphic.style.transition = time + 's linear';
-container.style.left = 'calc(50vw - ' + containerLeft + 'px)';
-container.style.top = 'calc(50vh - ' + containerTop + 'px)';
+container.style.left = containerLeft + 'px';
+container.style.top = containerTop + 'px';
 var graphicLeft = containerLeft * 1.3;
 var graphicTop = containerTop * 1.3;
-graphic.style.left = 'calc(50vw - ' + graphicLeft + 'px)';
-graphic.style.top = 'calc(50vh - ' + graphicTop + 'px';
+graphic.style.left = graphicLeft + 'px';
+graphic.style.top = 'calc(' + graphicTop + 'px - 30vh)';
 graphic.style.transform = 'scale(' + size + ')';
-size = Math.round(Math.random() * 0.2) + 3;
+size = Math.round(Math.random() * 0.2) + 2;
 container.style.transform = 'scale(' + size + ')';
 time = time * 1000;
 setTimeout(function() {
@@ -34,26 +34,26 @@ function moveContainer() {
 	currentLeft = containerLeft;
 	currentTop = containerTop;
 	while (containerLeft == currentLeft) {
-		containerLeft = Math.round(Math.random() * (container.clientWidth - 1) / 5) + 1 - (container.clientWidth / 2);
+		containerLeft = Math.round(Math.random() * (container.clientWidth - 1) / 5) + 1 - (container.clientWidth / 10);
 	}
 	console.log('before: ' + containerTop + ' ' + currentTop);
 	while (containerTop == currentTop) {
-		containerTop = Math.round(Math.random() * (container.clientHeight - 1) / 5) + 1 - (container.clientHeight / 2);
+		containerTop = Math.round(Math.random() * (container.clientHeight - 1) / 5) + 1 - (container.clientHeight / 10);
 	}
 	console.log('after: ' + containerTop + ' ' + currentTop);
 	containerDistance = (containerLeft - currentLeft) * (containerLeft - currentLeft) + (containerTop - currentTop) * (containerTop - currentTop);
-	time = containerDistance / 200000;
-	size = Math.round(Math.random() * 0.3) + 0.85;
+	time = Math.sqrt(containerDistance) / 30;
+	size = Math.round(Math.random() * 0.5) + 0.8;
 	container.style.transition = time + 's linear';
 	graphic.style.transition = time + 's linear';
-	container.style.left = 'calc(50vw - ' + containerLeft + 'px)';
-	container.style.top = 'calc(50vh - ' + containerTop + 'px)';
+	container.style.left = containerLeft + 'px';
+	container.style.top = containerTop + 'px';	
 	graphicLeft = containerLeft * 1.3;
 	graphicTop = containerTop * 1.3;
-	graphic.style.left = 'calc(50vw - ' + graphicLeft + 'px)';
-	graphic.style.top = 'calc(50vh - ' + graphicTop + 'px';
+	graphic.style.left = graphicLeft + 'px';
+	graphic.style.top = 'calc(' + graphicTop + 'px - 30vh)';
 	graphic.style.transform = 'scale(' + size + ')';
-	size = Math.round(Math.random() * 0.2) + 3;
+	size = Math.round(Math.random() * 0.2) + 2;
 	container.style.transform = 'scale(' + size + ')';
 	time = time * 1000;
 	setTimeout(function() {
