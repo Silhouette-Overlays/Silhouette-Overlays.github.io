9afaf03aec2d2bd5fbecfce654a3879d254771eb
Isaiah Meek <50851883+Isaiah-DSW@users.noreply.github.com>
2020-04-26 18:26:15 +1200

    Update script.js

--- logo-display/the-container/script.js
+++ logo-display/the-container/script.js
@@ -1,8 +1,8 @@
-window.onload = function() {
+/*window.onload = function() {
 	setInterval(function() {
 		document.getElementById('logos').style.animation = 'swing 1s ease';
 		setTimeout(function() {
 			document.getElementById('logos').style.animation = 'none';
 		}, 1000);
 	}, 25000);
-}
+}*/
