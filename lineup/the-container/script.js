792c6a55a71bd347f24b98c1c5f821da85d8fba5
Isaiah Meek <50851883+Isaiah-DSW@users.noreply.github.com>
2020-04-30 10:46:19 -1100

    Add files via upload

--- lineup/the-container/script.js
+++ lineup/the-container/script.js
@@ -10,25 +10,27 @@ function getParameterByName(name, url) {
 
 var id = getParameterByName('i');
 
-if (id == null || id == '') {
-	id = 0;
-}
-
 var i = 0;
 
-while (i + 1 <= id) {
-	document.getElementsByClassName('id')[i].style.opacity = 1;
-	i++;
-}
-
-
-window.onload = function() {
-	document.getElementsByClassName('id')[id].style.transition = 'opacity 1s';
-	setTimeout(function() {
-		document.getElementById('mover').style.height = '100vh';
-		id = parseInt(id);
+if (id != null && id != '') {
+	while (i + 1 <= id) {
+		document.getElementsByClassName('id')[i].style.opacity = 1;
+		i++;
+	}
+	window.onload = function() {
+		document.getElementsByClassName('id')[id].style.transition = 'opacity 1s';
 		setTimeout(function() {
-			document.getElementsByClassName('id')[id].style.opacity = '1';
-		}, 2000);
-	}, 1000);
+			document.getElementById('mover').style.height = '100vh';
+			id = parseInt(id);
+			setTimeout(function() {
+				document.getElementsByClassName('id')[id].style.opacity = '1';
+			}, 2000);
+		}, 1000);
+	}
+} else {
+	window.onload = function() {
+		setTimeout(function() {
+			document.getElementById('mover').style.height = '100vh';
+		}, 1000);
+	}
 }
